2fa5afe58ef506831438151a70baf5e2332cdc85
Javier Medina Lopez <franciscojavier.medina.lopez.contractor@bbva.com>
2020-05-24 13:59:48 +0200

    hello world with git push

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Heroku var to get a port number
 const PORT = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send('Hello World from git push!');
 });
 
 app.listen(PORT, function () {
