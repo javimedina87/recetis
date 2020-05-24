f332dac1ad9b25960549149c706b96b80273ca21
Javier Medina Lopez <franciscojavier.medina.lopez.contractor@bbva.com>
2020-05-24 13:54:50 +0200

    express server

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log('index js del trottis')
+const express = require('express');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
