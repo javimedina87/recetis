b68923a87fb02c11f4f8b20ba542d6bf272a1465
Javier Medina Lopez <franciscojavier.medina.lopez.contractor@bbva.com>
2020-05-24 21:11:56 +0200

    service worker and flex

---
+++ public/service-worker-recetas.js
@@ -0,0 +1,33 @@
+const cacheName = 'cache-v1-recetas';
+const precacheResources = [
+	'/',
+	'index.html',
+	'css/styles.css',
+	'images/backgroundHeader.jpg',
+];
+
+self.addEventListener('install', (event) => {
+	console.log('Service worker recetas install event!');
+	event.waitUntil(
+		caches.open(cacheName)
+			.then((cache) => {
+				return cache.addAll(precacheResources);
+			}),
+	);
+});
+
+self.addEventListener('activate', (event) => {
+	console.log('Service worker recetas activate event!');
+});
+
+self.addEventListener('fetch', (event) => {
+	console.log('Fetch intercepted for:', event.request.url);
+	event.respondWith(caches.match(event.request)
+		.then((cachedResponse) => {
+			if (cachedResponse) {
+				return cachedResponse;
+			}
+			return fetch(event.request);
+		}),
+	);
+});
--- server.js
+++ server.js
@@ -10,11 +10,13 @@ const app = express();
 const publicDir = path.join(__dirname, 'public'); // public directory
 
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public')); // mandatory to CSS works
 // app.use(logger('dev')); // TODO estudiarlo
 // app.use(bodyParser.json()); // parses json, multi-part (file), url-encoded
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(publicDir, 'index.html')); // start with index.thml
+	res.sendFile(path.join(publicDir, 'index.html')); // start with index.html
 });
 
 // Reload dependency code
