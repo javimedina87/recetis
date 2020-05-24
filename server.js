8aafcb82bbe8b5ddb7b8d1a1357e2b4de85d3902
Javier Medina Lopez <franciscojavier.medina.lopez.contractor@bbva.com>
2020-05-24 14:15:38 +0200

    npm start

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 // Heroku var to get a port number
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000
 
 app.get('/', function (req, res) {
     res.send('Hello World from git push 2!');
 });
 
 app.listen(PORT, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port: ', PORT);
 });
