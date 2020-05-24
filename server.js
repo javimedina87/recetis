20c32ea5196c0aa1529096c6244982dae94faedf
Javier Medina Lopez <franciscojavier.medina.lopez.contractor@bbva.com>
2020-05-24 13:57:15 +0200

    port var from heroku

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('Example app listening on port 3000!');
 });
