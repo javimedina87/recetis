9e78f05aef059c0b845e019712679cf9737b1da7
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-05-24 22:17:27 +0200

    add changes andrea

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 // Reload dependency code
 reload(app).then(function () {
 	http.createServer(app).listen(app.get('port'), function () {
-		console.log('Server listening on port ' + app.get('port'));
+		console.log('Server listening on localhost: ' + app.get('port'));
 	});
 }).catch(function (err) {
 	console.error('Reload could not start server', err);
