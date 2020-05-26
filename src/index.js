69a8a611229ec5066729b2356188f284c4a34fdb
Javier Medina Lopez <franciscojavier.medina.lopez.contractor@bbva.com>
2020-05-26 22:15:24 +0200

    testing

--- src/index.js
+++ src/index.js
@@ -15,5 +15,5 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 
-// serviceWorker.unregister(); // Deactivate service worker
-serviceWorker.register(); // Activate service worker
+serviceWorker.unregister(); // Deactivate service worker
+//serviceWorker.register(); // Activate service worker
