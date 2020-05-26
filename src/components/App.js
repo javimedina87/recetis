7343680fe1c80bb1984a2d97fe12f0e4e2baf6e0
Javier Medina Lopez <franciscojavier.medina.lopez.contractor@bbva.com>
2020-05-26 21:01:47 +0200

    Migrate app to React

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Banner from './Banner';
+import '../App.css';
+
+function App() {
+	return (
+		<div>
+            <Banner
+                imageName='egg.png'
+                text='Recetis para tod@s'/>
+
+			{/*Container (all app content except banner)*/}
+			<div className="container">
+
+				<h3>App migrada a React</h3>
+
+				<h5>Ideas</h5>
+				<ul>
+					<li>Añadir nueva receta</li>
+					<li>Top ranking</li>
+					<li>Imágenes de recetas</li>
+					<li>Últimas recetas añadidas</li>
+					<li>Imagen inicial con "Postre" y otra con "Platos"</li>
+					<li>Pasos a seguir una vez se escoge una receta</li>
+					<li>Cuando estas siguiendo los pasos de la receta... darle a un botón y que sea "siguiente paso". También un desplegable con los ingredientes</li>
+					<li>Puntuación y color de fondo de la receta</li>
+				</ul>
+
+			</div>
+
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Banner = (props) => {
+	return (
+		<header className="banner">
+			<img className="egg rotate-image" src={`${process.env.PUBLIC_URL}/${props.imageName}`} alt="" />
+			<span className="header-text">{props.text}</span>
+		</header>
+	)
+}
+
+Banner.propTypes = {
+	imageName: PropTypes.string,
+	text: PropTypes.string
+}
+
+export default Banner;
