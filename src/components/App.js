de69a08a690c5562f350abe80cc448d33fad9cb4
AndreaFCR <andrea.clavijo.r@gmail.com>
2020-05-27 19:07:56 +0200

    prueba

--- src/components/App.js
+++ src/components/App.js
@@ -1,60 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import Banner from './Banner';
-import ListItems from './ListItems';
-import SmartButton from './SmartButton';
-import '../App.css';
+import React, { useEffect, useState } from "react";
+import Banner from "./Banner";
+import ListItems from "./ListItems";
+import SmartButton from "./SmartButton";
+import "../App.css";
 
-const backendApiIdeasUrl = 'https://recetis-backend.herokuapp.com/api/ideas';
+const backendApiIdeasUrl = "https://recetis-backend.herokuapp.com/api/ideas";
 
 function App() {
-	const [ideas, setIdeas] = useState([]);
+  const [ideas, setIdeas] = useState([]);
 
-	const addNewIdea = (e) => {
-		e.preventDefault(); // TODO check this
+  const addNewIdea = (e) => {
+    e.preventDefault(); // TODO check this
 
-		const url = backendApiIdeasUrl;
-		const data = { name: document.getElementById('newIdeaText').value };
+    const url = backendApiIdeasUrl;
+    const data = { name: document.getElementById("newIdeaText").value };
 
-		fetch(url, {
-			method: 'POST',
-			body: JSON.stringify(data),
-			headers:{
-				'Content-Type': 'application/json'
-			}
-		}).then(res => res.json())
-			.catch(error => console.error('Error: ', error))
-			.then(response => console.log('Success: ', response));
-	}
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error("Error: ", error))
+      .then((response) => console.log("Success: ", response));
+  };
 
-	useEffect(() => {
-		fetch(backendApiIdeasUrl)
-			.then((response) => response.json()
-			.then((ideas) => {
-				console.log('ideas: ', ideas);
+  useEffect(() => {
+    fetch(backendApiIdeasUrl).then((response) =>
+      response.json().then((ideas) => {
+        console.log("ideas_holaa: ", ideas);
 
-				setIdeas(ideas);
-			}));
-	}, []);
+        setIdeas(ideas);
+      })
+    );
+  }, []);
 
-	return (
-		<div>
-            <Banner
-                imageName='egg.png'
-                text='Recetis para tod@s'/>
+  return (
+    <div>
+      <Banner imageName="egg.png" text="Recetis para tod@s" />
 
-			{/*Container (all app content except banner)*/}
-			<div className='container'>
+      {/*Container (all app content except banner)*/}
+      <div className="container">
+        <span>Lista de ideas (Componente ListItems.js)</span>
 
-				<span>Lista de ideas (Componente ListItems.js)</span>
+        <ListItems items={ideas} />
+      </div>
 
-				<ListItems items={ideas}/>
-			</div>
-
-			<input id='newIdeaText' type='text' />
-			<SmartButton handleClick={addNewIdea} visualName='+'/>
-
-		</div>
-	);
+      <input id="newIdeaText" type="text" />
+      <SmartButton handleClick={addNewIdea} visualName="+" />
+    </div>
+  );
 }
 
 export default App;
